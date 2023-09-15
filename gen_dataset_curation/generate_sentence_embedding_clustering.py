89b61913a11ac96537b8e00e92a4bc61e735a181
minju967 <nayohan13@gmail.com>
2023-09-15 14:49:21 +0900

    [MOD] Step3,4 gen sentence emb & clustering & sampling

--- gen_dataset_curation/generate_sentence_embedding_clustering.py
+++ gen_dataset_curation/generate_sentence_embedding_clustering.py
@@ -1,27 +1,18 @@
 import os
-
-import tqdm
-import pandas as pd
-import numpy as np
-import torch
-# import datasets
 import copy
 import argparse
-import glob
-from glob import glob
+
+import torch
+
+import numpy as np
+import pandas as pd
+
 from tqdm import tqdm
-
-from datasets import load_dataset
-from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
-from transformers import AutoTokenizer,AutoModel
-
-from yellowbrick.cluster import KElbowVisualizer
+from transformers import AutoTokenizer, AutoModel
 from sklearn.cluster import KMeans
 from datasets import Dataset, DatasetDict
 
-
-class DataSelect:
+class SentenceEmbeddingClustering:
     def __init__(self, df):
         self.df = df
 
@@ -61,121 +52,51 @@ class DataSelect:
         return temp
 
 
-def concat_df_list(new_df_list): # length 별 쪼개진 5개 concat
-    final_df = pd.concat(new_df_list, ignore_index=True)
-    return final_df
-
-def sampling_data(df_list, K, M, N):
-    new_df_list = []
-    for df in df_list:
-        if M == 'rand':
-            df = df.sample(frac=N)
-        elif M == 'ppl':
-            df = df.sort_values('ppl')
-            df_cluster_list = [df['cluster']==k for k in range(K)]
-            clustered_df_list = [df[df_bool] for df_bool in df_cluster_list]
-            
-            clustered_ppl_df_list = []
-            for clustered_df in clustered_df_list:
-                small_range = list(range(0, len(clustered_df), int(1 / N)))
-                clustered_ppl_df = clustered_df.iloc[small_range, :]
-                clustered_ppl_df_list.append(clustered_ppl_df)
-            df = pd.concat(clustered_ppl_df_list, ignore_index=True)
-            
-        elif M == 'ppl_h':
-            df = df.sort_values('ppl', ascending=False)
-            df_cluster_list = [df['cluster']==k for k in range(K)]
-            clustered_df_list = [df[df_bool] for df_bool in df_cluster_list]
-            
-            clustered_ppl_df_list = []
-            for clustered_df in clustered_df_list:
-                prop = int(N * len(clustered_df))
-                clustered_df = clustered_df.drop_duplicates(['ppl'])
-                print('c:', clustered_df)
-                clustered_ppl_df = clustered_df[:prop]
-                clustered_ppl_df_list.append(clustered_ppl_df)
-            df = pd.concat(clustered_ppl_df_list, ignore_index=True)
-
-        new_df_list.append(df)
-    return new_df_list
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process data using DataSelect class")
-    parser.add_argument("--len", type=int, help="make_split_len")
-    parser.add_argument("--k", type=int, help="Number of clusters for task 'run_clustering'")
-    # parser.add_argument("--m", type=str, help="Sampling method for task 'sampling_data'")
-    # parser.add_argument("--n", type=float, help="Sampling ratio for task 'sampling_data'")
-    parser.add_argument("--device", type=int, default=0, help="Gpu device number")
+    parser.add_argument("--len", type=int, default=10, help="make_split_len_group")
+    parser.add_argument("--device", type=int, default=0, help="gpu device number")
     args = parser.parse_args()
     
     os.environ["CUDA_VISIBLE_DEVICES"] = str(args.device)
-    # len_values = [1, 5]
     k_values = [1, 10, 20, 50, 100]
-    m_values = ['rand']#, 'ppl_h']
-    n_values = [0.01]#, 0.05, 0.1]
+    m_values = ['rand', 'ppl', 'ppl_h']
+    n_values = [0.01, 0.05, 0.1]
 
-
+    dataset_name = 'nayohan/koquality_raw'
+    model_name = 'EleutherAI/polyglot-ko-1.3b'
+    embedding_model_name = 'BM-K/KoSimCSE-roberta-multitask'
+    ppl_dataset_path = './result_ppl/'
     
-    # 3. 임베딩 생성 및 길이기반 클러스터링
-    # ppl_dataset_path = '/home/uj-user/Yo/HiT5/HCLT/gen_perplexity/result_ppl/'
-    # ppl_df = pd.read_json(f'{ppl_dataset_path}koquality_raw_ppl_polyglot-ko-1.3b_nospace.json', lines=True, orient='records')
-    # # ppl_df = ppl_df.loc[:10000, :]
+    # 3. generate sentence embedding and length(L) based clustering(K)
+    datasetname = dataset_name.split('/')[-1]
+    modelname = model_name.split('/')[-1]
+    ppl_df = pd.read_json(  f"./{ppl_dataset_path}/{datasetname}_ppl_{modelname}.json", lines=True, orient='records')
+    # ppl_df = ppl_df.loc[:10000, :]
     # ppl_df['len_group']=""
-    # calc = DataSelect(ppl_df)
     
-    # device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    # model = AutoModel.from_pretrained('BM-K/KoSimCSE-roberta-multitask').to(device)
-    # tokenizer = AutoTokenizer.from_pretrained('BM-K/KoSimCSE-roberta-multitask')
+    calc = SentenceEmbeddingClustering(ppl_df)
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model = AutoModel.from_pretrained(embedding_model_name).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(embedding_model_name)
     
-    # split_len_df_list = calc.make_split_len(args.len)
-    # np_emb_all_list = calc.make_embedding(calc.make_split_len(args.len), tokenizer, model, device)
+    split_len_df_list = calc.make_split_len(args.len)
+    np_emb_all_list = calc.make_embedding(calc.make_split_len(args.len), tokenizer, model, device)
     
-    # for k_val in k_values:
-    #     clustered_df_list = calc.run_clustering(args.len, split_len_df_list, np_emb_all_list, k_val)
-    #     final_df = pd.concat(clustered_df_list, ignore_index=True)
-    #     final_df = final_df.sort_values(by=['len_group', 'cluster', 'ppl', 'len'], axis=0)
-    #     final_df = final_df[["ppl", "len", "len_group", "cluster", "group", "instruction", "output"]]
+    for k_val in k_values:
+        clustered_df_list = calc.run_clustering(args.len, split_len_df_list, np_emb_all_list, k_val)
+        final_df = pd.concat(clustered_df_list, ignore_index=True)
+        final_df = final_df.sort_values(by=['len_group', 'cluster', 'ppl', 'len'], axis=0)
+        final_df = final_df[["ppl", "len", "len_group", "cluster", "group", "instruction", "output"]]
 
-    #     output_dir = f"result_ppl_instruction_len{args.len}"
-    #     os.makedirs(output_dir, exist_ok=True)
-    #     filename = f"result_len{args.len}_k{k_val}.json"
-    #     output_path = os.path.join(output_dir, filename)
-    #     final_df.to_json(output_path, orient='records', lines=True, force_ascii=False)
+        output_dir = f"result_ppl_instruction_len{args.len}"
+        os.makedirs(output_dir, exist_ok=True)
+        filename = f"result_len{args.len}_k{k_val}.json"
+        output_path = os.path.join(output_dir, filename)
+        final_df.to_json(output_path, orient='records', lines=True, force_ascii=False)
 
-        # upload to huggingface dataset
+        # # upload to huggingface dataset
         # raw_train = Dataset.from_pandas(final_df)
         # final_dataset = DatasetDict({'train': raw_train})
-        # final_dataset = final_dataset.remove_columns(['ppl  '])
+        # final_dataset = final_dataset.remove_columns(['ppl'])
         # final_dataset.push_to_hub(f'nayohan/koquality_len{args.len}_k{k_val}')
-        
-        
-    # 4. Method 샘플링 및 샘플링 N%
-    ppl_dataset_path = f'/home/uj-user/Yo/HiT5/HCLT/gen_dataset_curation/result_ppl_instruction_len{args.len}'
-    ppl_dataset_list = glob(ppl_dataset_path+'/*')
-    # print(ppl_dataset_list)
-    for k_val in k_values:
-        # print(f'{ppl_dataset_path}/result_len1_k{k_val}.json')
-        ppl_df = pd.read_json(f'{ppl_dataset_path}/result_len{args.len}_k{k_val}.json', lines=True, orient='records')
-        # print('ppl_df',ppl_df)
-        ppl_df_bool_list = [ppl_df['len_group']==len for len in range(args.len)]
-        # print(ppl_df_bool_list)
-        clustered_df_list = [ppl_df[df_bool] for df_bool in ppl_df_bool_list]
-        # print(clustered_df_list)
-        
-        for m_val in m_values:
-            for n_val in n_values:
-                sampled_df_list = sampling_data(clustered_df_list, k_val, m_val, n_val)
-                result = concat_df_list(sampled_df_list)
-                result = result.sort_values(by=['len_group', 'cluster', 'ppl', 'len'], axis=0)
-                # 결과를 저장할 디렉토리 생성
-                output_dir = f"final_results"
-                os.makedirs(output_dir, exist_ok=True)
-
-                # 파일 이름 생성
-                filename = f"result_len{args.len}_k{k_val}_m{m_val}_n{n_val}.json"
-                output_path = os.path.join(output_dir, filename)
-
-                # 데이터프레임을 JSON 파일로 저장
-                # final_df = final_df[["ppl", "len", "len_group", "cluster", "group", "instruction", "output"]]
-                # final_result = pd.DataFrame({"cluster": result['cluster'], "ppl": result['ppl'], "instruction": result['instruction'], "output": result['output']})
-                result.to_json(output_path, orient='records', lines=True, force_ascii=False)
---
+++ gen_dataset_curation/proceed_sampling_dataset.py
@@ -0,0 +1,70 @@
+import os
+import argparse
+import pandas as pd
+
+def sampling_data(df_list, K, M, N):
+    new_df_list = []
+    for df in df_list:
+        if M == 'rand':
+            df = df.sample(frac=N)
+        elif M == 'ppl':
+            df = df.sort_values('ppl')
+            df_cluster_list = [df['cluster']==k for k in range(K)]
+            clustered_df_list = [df[df_bool] for df_bool in df_cluster_list]
+            
+            clustered_ppl_df_list = []
+            for clustered_df in clustered_df_list:
+                small_range = list(range(0, len(clustered_df), int(1 / N)))
+                clustered_ppl_df = clustered_df.iloc[small_range, :]
+                clustered_ppl_df_list.append(clustered_ppl_df)
+            df = pd.concat(clustered_ppl_df_list, ignore_index=True)
+            
+        elif M == 'ppl_h':
+            df = df.sort_values('ppl', ascending=False)
+            df_cluster_list = [df['cluster']==k for k in range(K)]
+            clustered_df_list = [df[df_bool] for df_bool in df_cluster_list]
+            
+            clustered_ppl_df_list = []
+            for clustered_df in clustered_df_list:
+                prop = int(N * len(clustered_df))
+                clustered_df = clustered_df.drop_duplicates(['ppl'])
+                print('c:', clustered_df)
+                clustered_ppl_df = clustered_df[:prop]
+                clustered_ppl_df_list.append(clustered_ppl_df)
+            df = pd.concat(clustered_ppl_df_list, ignore_index=True)
+
+        new_df_list.append(df)
+    return new_df_list
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="Process data using DataSelect class")
+    parser.add_argument("--len", type=int, default=10, help="make_split_len_group")
+    parser.add_argument("--device", type=int, default=0, help="gpu device number")
+    args = parser.parse_args()
+    
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.device)
+    k_values = [1, 10, 20, 50, 100]
+    m_values = ['rand', 'ppl', 'ppl_h']
+    n_values = [0.01, 0.05, 0.1]
+
+    ppl_dataset_path = f'./result_ppl_instruction_len{args.len}'
+
+    # 4. Sampling by method(M) with sampling ratio (N)
+    for k_val in k_values:
+        ppl_df = pd.read_json(f'{ppl_dataset_path}/result_len{args.len}_k{k_val}.json', lines=True, orient='records')
+        ppl_df_bool_list = [ppl_df['len_group']==len for len in range(args.len)] # load len_group
+        clustered_df_list = [ppl_df[df_bool] for df_bool in ppl_df_bool_list] 
+        
+        for m_val in m_values:
+            for n_val in n_values:
+                sampled_df_list = sampling_data(clustered_df_list, k_val, m_val, n_val)
+                result =  pd.concat(sampled_df_list, ignore_index=True)
+                result = result.sort_values(by=['len_group', 'cluster', 'ppl', 'len'], axis=0)
+                
+                # 결과를 저장할 디렉토리 생성
+                output_dir = f"final_results"
+                os.makedirs(output_dir, exist_ok=True)
+                filename = f"result_len{args.len}_k{k_val}_m{m_val}_n{n_val}.json"
+                output_path = os.path.join(output_dir, filename)
+                result.to_json(output_path, orient='records', lines=True, force_ascii=False)
